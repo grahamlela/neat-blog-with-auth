d0d3412449376b3ce2c8fea2edc210fd827b70c7
Graham <grahamogunlela@gmail.com>
2025-03-10 23:28:57 +0100

    Remove dotenv dependency in main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import *
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 
 '''
@@ -27,7 +27,7 @@ pip3 install -r requirements.txt
 
 This will install the packages from the requirements.txt for this project.
 '''
-load_dotenv()
+# load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("FLASK_KEY")
